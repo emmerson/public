f8549e04872238b6c26406e0c0dea4325290267b
Andrew McWatters <mcwattersandrew@gmail.com>
2014-09-06 16:24:00 -0700

    Updates to Axis page and added résumé

---
+++ app/scripts/axis.js
@@ -0,0 +1,21 @@
+/*!
+ * /public
+ * From the desk of Andrew McWatters
+ * http://www.andrewmcwatters.com/
+ * @author Andrew McWatters
+ * Copyright 2014.
+ */
+(function ($, window, document, undefined) {
+    'use strict';
+
+    var axis = window.axis;
+
+    axis = (function () {
+        return {};
+    })();
+
+    window.axis = axis;
+
+    $(function() {
+    });
+})(Zepto || jQuery, window, document);
