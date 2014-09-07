9ef93a45bc8a3ed19adf01554c037f73c624e15d
Andrew McWatters <mcwattersandrew@gmail.com>
2014-09-06 17:22:56 -0700

    Updated Gruntfile.js

--- Gruntfile.js
+++ Gruntfile.js
@@ -191,8 +191,10 @@ module.exports = function (grunt) {
         dest: '<%= config.dist %>'
       },
       html: [
+        '<%= config.app %>/404.html',
         '<%= config.app %>/index.html',
         '<%= config.app %>/axis/index.html'
+        '<%= config.app %>/axis/account.html'
       ]
     },
 
