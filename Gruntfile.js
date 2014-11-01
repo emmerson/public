273b72027410d0ae63f54db973ac7bf5c648b5e8
Andrew McWatters <mcwattersandrew@gmail.com>
2014-10-31 20:46:24 -0700

    Reorganizing routes and landing page design

--- Gruntfile.js
+++ Gruntfile.js
@@ -69,7 +69,7 @@ module.exports = function (grunt) {
     connect: {
       options: {
         port: 9000,
-        open: true,
+        open: 'http://localhost:<%= connect.options.port %>',
         livereload: 35729,
         // Change this to '0.0.0.0' to access the server from outside
         hostname: 'localhost'
