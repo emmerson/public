3b6e280ef0666bc37b64beb47616d950c3e8861a
Andrew McWatters <mcwattersandrew@gmail.com>
2014-11-16 00:02:55 -0700

    Updates to header

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,27 @@
-console.log('\'Allo \'Allo!');
+/*!
+ * /public
+ * From the desk of Andrew McWatters
+ * http://www.andrewmcwatters.com/
+ * @author Andrew McWatters
+ * @version 0.0.0
+ * Copyright 2014. MIT licensed.
+ */
+;(function ($, window, document, undefined) {
+  'use strict';
+
+  var page        = window.page;
+  var queryString = window.queryString;
+  var location    = window.location;
+
+  $(function() {
+    /**
+     * Set query object.
+     */
+    page(function(ctx, next) {
+      ctx.query = queryString.parse(location.search);
+      next();
+    });
+
+    page();
+  });
+})(Zepto || jQuery, window, document);
