1e682d7c3d143c13b5adfc3aaa0becc99438893e
Andrew McWatters <mcwattersandrew@gmail.com>
2014-11-27 13:07:45 -0700

    Updated navLink directive to set nav title when found

--- app/scripts/directives/navlink.js
+++ app/scripts/directives/navlink.js
@@ -7,13 +7,18 @@
  * # navLink
  */
 angular.module('publicApp')
-  .directive('navLink', function ($location) {
+  .directive('navLink', function ($location, $document) {
     return {
       restrict: 'C',
       link: function postLink(scope, element) {
         var routeName = $location.path();
-        if (routeName === element.find('a').attr('href')) {
+        var link      = element.find('a');
+        if (routeName === link.attr('href')) {
           element.addClass('active');
+
+          var document = $document[0];
+          var navTitle = document.getElementById('nav-title');
+          navTitle.textContent = link.attr('title');
         }
       }
     };
