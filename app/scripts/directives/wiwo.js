e7be81695501b08230afcf33cf1076eeb84c5626
Andrew McWatters <mcwattersandrew@gmail.com>
2014-12-03 14:43:14 -0700

    Initial wiwo rewrite

---
+++ app/scripts/directives/wiwo.js
@@ -0,0 +1,69 @@
+'use strict';
+
+/**
+ * @ngdoc directive
+ * @name publicApp.directive:wiwo
+ * @description
+ * # wiwo
+ */
+angular.module('publicApp')
+  .directive('wiwo', function ($window, $rootScope) {
+    /**
+     * Module dependencies.
+     */
+
+    var TimelineMax  = $window.TimelineMax;
+
+    return {
+      restrict: 'C',
+      link: function postLink(scope, element) {
+        /**
+         * Animate "wiwo" route.
+         */
+
+        function animateWiwo() {
+          // initialize timeline and retrieve elements to animate
+          var tl    = new TimelineMax();
+          var h1    = element[0].getElementsByTagName('h1');
+          var intro = element[0].getElementsByClassName('intro');
+          var tiles = element[0].getElementsByClassName('project-tile');
+          tl
+            // wait for main to stagger in
+            .delay(0.2)
+            // fade in main
+            .call(function() { element.addClass('in'); })
+            // fade in and slide down header
+            .staggerFromTo([h1, intro], 0.4, {
+                opacity: 0,
+                y: -9
+              }, {
+                opacity: 1,
+                y: 0
+              }, 0.2)
+            // rotate in project tiles
+            .staggerFromTo(tiles, 0.6, {
+                opacity: 0,
+                rotationX: 90,
+                transformOrigin: '50% 50% -152px'
+              }, {
+                opacity: 1,
+                rotationX: 0,
+                transformOrigin: '50% 50% -152px'
+              }, 0.2, '-=0.4');
+        }
+
+        /**
+         * Handle "initialize" event.
+         */
+
+        if ($rootScope.initialized) {
+          animateWiwo();
+        } else {
+          var remove = scope.$on('initialize', function() {
+            remove();
+            animateWiwo();
+          });
+        }
+      }
+    };
+  });
