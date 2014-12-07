bda58ab178fe10617790b62f99df7aa32216970e
Andrew McWatters <mcwattersandrew@gmail.com>
2014-12-07 16:31:23 -0700

    Created new generator-angular project

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -10,12 +10,7 @@
  */
 angular
   .module('publicApp', [
-    'ngAnimate',
-    'ngCookies',
-    'ngResource',
-    'ngRoute',
-    'ngSanitize',
-    'ngTouch'
+    'ngRoute'
   ])
   .config(function ($routeProvider) {
     $routeProvider
