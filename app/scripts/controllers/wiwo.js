a4e49e4709f1143230b693aea2481d5c72e34dba
Andrew McWatters <mcwattersandrew@gmail.com>
2014-12-15 14:12:17 -0700

    Added wiwo page

--- app/scripts/controllers/wiwo.js
+++ app/scripts/controllers/wiwo.js
@@ -9,9 +9,46 @@
  */
 angular.module('publicApp')
   .controller('WiwoCtrl', function ($scope) {
-    $scope.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
+    $scope.projects = [
+      {
+        type: 'Organization',
+        name: 'Planimeter',
+        logo: {
+          src: 'images/logo_planimeter.svg',
+          width: 215
+        },
+        since: 2014,
+        what: 'International Development Team'
+      },
+      {
+        type: 'Game',
+        name: 'Vertex Adventure',
+        logo: {
+          src: 'images/logo_vadventure.svg',
+          width: 215
+        },
+        since: 2013,
+        what: '2D Fantasy MMORPG'
+      },
+      {
+        type: 'Engine',
+        name: 'Grid',
+        logo: {
+          src: 'images/logo_grid.svg',
+          width: 122
+        },
+        since: 2013,
+        what: '2D Game Engine'
+      },
+      {
+        type: 'Platform',
+        name: 'Axis',
+        logo: {
+          src: 'images/logo_axis.svg',
+          width: 117
+        },
+        since: 2013,
+        what: 'Game Account Management System'
+      }
     ];
   });
---
+++ app/scripts/directives/wiwo.js
@@ -0,0 +1,65 @@
+'use strict';
+
+/**
+ * @ngdoc directive
+ * @name publicApp.directive:wiwo
+ * @description
+ * # wiwo
+ */
+angular.module('publicApp')
+  .directive('wiwo', function ($window, $rootScope) {
+    /**
+     * Module dependencies.
+     */
+
+    var TimelineMax = $window.TimelineMax;
+
+    return {
+      restrict: 'C',
+      link: function postLink(scope, element) {
+        /**
+         * Animate "wiwo" route.
+         */
+
+        function animateWiwo() {
+          // initialize timeline and retrieve elements to animate
+          var tl    = new TimelineMax();
+          var h1    = element[0].getElementsByTagName('h1');
+          var intro = element[0].getElementsByTagName('p');
+          var tiles = element[0].getElementsByClassName('project-tile');
+          tl
+            // wait for main to stagger in
+            .delay(0.15)
+            // bring forth header and intro
+            .staggerFromTo([h1, intro], 0.4, {
+                opacity: 0,
+                z: -141,
+              }, {
+                opacity: 1,
+                z: 0
+              }, 0.06)
+            // bring forth project tiles
+            .staggerFromTo(tiles, 0.6, {
+                opacity: 0,
+                z: -141,
+              }, {
+                opacity: 1,
+                z: 0,
+              }, 0.06, '-=0.4');
+        }
+
+        /**
+         * Handle "initialize" event.
+         */
+
+        if ($rootScope.initialized) {
+          requestAnimationFrame(animateWiwo);
+        } else {
+          var remove = scope.$on('initialize', function() {
+            remove();
+            requestAnimationFrame(animateWiwo);
+          });
+        }
+      }
+    };
+  });
