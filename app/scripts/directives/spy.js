564908bf0954b24cf286615c47850bca3537878c
Andrew McWatters <mcwattersandrew@gmail.com>
2014-12-21 22:01:15 -0700

    Updated portfolio view

---
+++ app/scripts/directives/spy.js
@@ -0,0 +1,31 @@
+'use strict';
+
+/**
+ * @ngdoc directive
+ * @name publicApp.directive:spy
+ * @description
+ * # spy
+ */
+angular.module('publicApp')
+  .directive('spy', function () {
+    return {
+      restrict: 'A',
+      link: function postLink(scope, element, attrs) {
+        requestAnimationFrame(function() {
+          var spy = attrs.spy;
+
+          /**
+           * Affix spy.
+           */
+
+          if (spy === 'affix') {
+            var offset = {};
+            if (element.data('offset-top'))    { offset.top    = +element.data('offset-top'); }
+            if (element.data('offset-bottom')) { offset.bottom = +element.data('offset-bottom'); }
+            console.log(offset);
+            element.affix({offset: offset});
+          }
+        });
+      }
+    };
+  });
---
+++ test/spec/directives/spy.js
@@ -0,0 +1,19 @@
+'use strict';
+
+describe('Directive: spy', function () {
+
+  // load the directive's module
+  beforeEach(module('publicApp'));
+
+  var element,
+    scope;
+
+  beforeEach(inject(function ($rootScope) {
+    scope = $rootScope.$new();
+  }));
+
+  it('should make hidden element visible', inject(function ($compile) {
+    element = angular.element('<div data-spy></div>');
+    element = $compile(element)(scope);
+  }));
+});
