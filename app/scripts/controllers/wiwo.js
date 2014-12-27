1da6d33e427f3434dedab1c878df438bc966b3af
Andrew McWatters <mcwattersandrew@gmail.com>
2014-12-27 00:00:52 -0700

    Updated wiwo controller

--- app/scripts/controllers/wiwo.js
+++ app/scripts/controllers/wiwo.js
@@ -15,43 +15,41 @@ angular.module('publicApp')
 
     $scope.projects = [
       {
-        id:     'planimeter',
-        new:    true,
-        type:   'Organization',
-        name:   'Planimeter',
-        logo:   'images/logo_planimeter.4ff78814.svg',
-        since:  2014,
-        what:   'International Development Team',
-        href:   '/planimeter',
-        target: '_self'
+        id:    'planimeter',
+        new:   true,
+        type:  'Organization',
+        name:  'Planimeter',
+        logo:  'images/logo_planimeter.4ff78814.svg',
+        since: 2014,
+        what:  'International Development Team',
+        href:  '/planimeter'
       },
       {
-        id:     'vadventure',
-        type:   'Game',
-        name:   'Vertex Adventure',
-        logo:   'images/logo_vadventure.dd082bfe.svg',
-        since:  2013,
-        what:   '2D Fantasy MMORPG',
-     // href:   '/vadventure',
-        target: '_self'
+        id:    'vadventure',
+        type:  'Game',
+        name:  'Vertex Adventure',
+        logo:  'images/logo_vadventure.dd082bfe.svg',
+        since: 2013,
+        what:  '2D Fantasy MMORPG',
+     // href:  '/vadventure'
       },
       {
-        id:     'grid',
-        type:   'Engine',
-        name:   'Grid',
-        logo:   'images/logo_grid.19de7e30.svg',
-        since:  2013,
-        what:   '2D Game Engine',
-     // href:   '/grid'
+        id:    'grid',
+        type:  'Engine',
+        name:  'Grid',
+        logo:  'images/logo_grid.19de7e30.svg',
+        since: 2013,
+        what:  '2D Game Engine',
+     // href:  '/grid'
       },
       {
-        id:     'axis',
-        type:   'Platform',
-        name:   'Axis',
-        logo:   'images/logo_axis.08eef562.svg',
-        since:  2013,
-        what:   'Game Account Management System',
-     // href:   '/axis'
+        id:    'axis',
+        type:  'Platform',
+        name:  'Axis',
+        logo:  'images/logo_axis.08eef562.svg',
+        since: 2013,
+        what:  'Game Account Management System',
+        href:  '/axis'
       }
     ];
   });
